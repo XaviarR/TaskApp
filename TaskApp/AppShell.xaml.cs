1326f817b82f4d74940e2844f1370db3ed77d50c
akamateina <teinacsa25@gmail.com>
2023-10-11 13:11:19 +1300

    Add project files.

---
+++ TaskApp/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskApp
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+
+			Routing.RegisterRoute(nameof(DetailPage), typeof(DetailPage));
+		}
+	}
+}
---
+++ TaskApp/DetailPage.xaml.cs
@@ -0,0 +1,12 @@
+using TaskApp.ViewModel;
+
+namespace TaskApp;
+
+public partial class DetailPage : ContentPage
+{
+	public DetailPage(DetailViewModel vm)
+	{
+		InitializeComponent();
+		BindingContext = vm;
+	}
+}
